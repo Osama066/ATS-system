c365d421a99d2142b688ae820e75b5b706c78c05
Osama066 <109853647+Osama066@users.noreply.github.com>
2024-01-17 21:05:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ from PIL import Image
 import pdf2image
 import google.generativeai as genai
 
-genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY")) #your api key from googlegemini should be save in .env 
 def get_gemini_response(input,pdf_content,prompt):
     model=genai.GenerativeModel("gemini-pro-vision")
     response=model.generate_content([input,pdf_content[0],prompt])
